8cb3144b28d6fb4ddf1f8b820c5c4d6e9fee4976
dikyy03 <139208200+dikyy03@users.noreply.github.com>
2024-05-15 14:32:07 +0800

    Add files via upload

---
+++ project_akhir/daftar_produk.php
@@ -0,0 +1,44 @@
+<!doctype html>
+<html lang="en">
+
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Nab Water</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+  </head>
+
+  <body>
+    <!-- Header -->
+    <?php include "header.php";?>
+    <!-- End Header -->
+
+<div class="container-lg">
+  <div class="row">
+    <!-- SideBar -->
+        <?php include "sidebar.php";?>
+    <!-- End Bar -->
+
+    <!-- Content -->
+    <div class="col-lg-9 mt-2">
+    <div class="card">
+  <div class="card-header">
+    Daftar Produk
+  </div>
+  <div class="card-body">
+    <h5 class="card-title">Welcome To NabWater</h5>
+    <p class="card-text">NAB Water adalah sebuah perusahaan depot air minum yang menggunakan teknologi Reverse Osmosis (RO)
+      untuk menyediakan air minum berkualitas tinggi kepada pelanggan. Dengan menggunakan proses RO, NAB Water mampu menyaring air 
+      dari berbagai sumber menjadi air bersih yang bebas dari kontaminan dan partikel-partikel yang tidak diinginkan.</p>
+    <a href="#" class="btn btn-primary">Beli Sekarang</a>
+  </div>
+</div>
+    </div>
+    <!-- End Content -->
+  </div>
+</div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+  </body>
+  
+</html>
