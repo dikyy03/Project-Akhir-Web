d41b3813f3f61a2a3bb4ee1f23cd48f8e0c3f8d8
dikyy03 <muhammaddicky690@gmail.com>
2024-06-12 21:56:36 +0800

    Membuat menu untuk kelola user

--- daftar_user.php
+++ daftar_user.php
@@ -6,9 +6,50 @@
                     Daftar User
                 </div>
                 <div class="card-body">
-                    <h5 class="card-title">Welcome Depot NabWater</h5>
-                    <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Error autem cumque officia odio velit dolorem quaerat officiis perferendis eos exercitationem, similique et nemo adipisci aliquam est, sed omnis modi! Amet ut doloribus magni consectetur labore harum! Totam consequatur dolorem quos officiis assumenda consectetur deleniti dolores reiciendis maxime laudantium illo sapiente, porro error quo, praesentium rem! Libero labore maxime assumenda eaque natus. Ducimus accusamus sequi, earum ratione illum nam quae velit. Soluta dolorum, eveniet aliquid consectetur quibusdam ipsum distinctio laborum excepturi corrupti, illo fugiat officiis beatae animi aut tempore illum ratione culpa nisi unde. Quaerat id quibusdam mollitia vero placeat repellendus.</p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                    <div class="table-responsive">
+                        <table class="table table-hover">
+                            <thead>
+                                <tr>
+                                    <th scope="col">No</th>
+                                    <th scope="col">Username</th>
+                                    <th scope="col">Email</th>
+                                    <th scope="col">Alamat</th>
+                                    <th scope="col">Nomor Telepon</th>
+                                    <th scope="col">Aksi</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php
+                                $query = "SELECT * FROM tbl_user";
+                                $result = $connection->query($query);
+
+                                if ($result->num_rows > 0) {
+                                    $no = 1;
+                                    while ($row = $result->fetch_assoc()) {
+                                        $no ?>
+                                        <tr>
+                                            <th scope="row"><?= $no ?> </th>
+                                            <td><?= $row['username'] ?> </td>
+                                            <td><?= $row['email'] ?></td>
+                                            <td><?= $row['alamat'] ?></td>
+                                            <td><?= $row['nomor_telepon'] ?></td>
+                                            <td class="d-flex">
+                                                <button class="btn btn-info btn-sm" data-bs-toggle="modal" data-bs-target="#exampleModal"><i class="bi bi-eye"></i></button>
+                                                <button class="btn btn-warning btn-sm"><i class="bi bi-pencil-square"></i></button>
+                                                <button class="btn btn-danger btn-sm"><i class="bi bi-trash"></i></i></button>
+                                            </td>
+                                        </tr>
+                                    <?php $no++;
+                                    }
+                                } else { ?>
+                                    <tr>
+                                        <td colspan="5">Tidak ada user</td>
+                                    </tr>
+                                <?php }
+                                ?>
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
         </div>
